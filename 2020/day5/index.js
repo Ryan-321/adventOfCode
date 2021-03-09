b8519468162dd0b2552c38324e8e8476e30428fb
Ryan Kahle <ryan@triblio.com>
2021-03-09 09:44:28 -0500

    passes assertions

--- 2020/day5/index.js
+++ 2020/day5/index.js
@@ -1,41 +1,48 @@
-const fs = require('fs');
-const assert = require('assert').strict;
+const fs = require("fs");
+const assert = require("assert").strict;
 
 const boardingPasses = fs
-  .readFileSync('./2020/day5/input.txt', 'utf-8')
-  .split('\n')
+  .readFileSync("./2020/day5/input.txt", "utf-8")
+  .split("\n")
   .map((str) => {
     const row = str.slice(0, 7);
     const column = str.slice(7);
     return [row, column];
   });
-// console.log({ boardingPasses });
-
-const dir = {
-  F: 'front',
-  B: 'back',
-  L: 'left',
-  R: 'right',
-};
-
-// remember rows start at 0, remember to add 1
-const rows = 127;
-const columns = 8;
 
 /**
- * Takes a range and returns the upper and lower halves of that range
+ * Takes a range and returns the upper and lower range depending on direction or returns the file row number
+ * 
  * @param {array} range number[]
- * @return {object} {lower: [number, number], upper: [number, number]}
+ * @param {string} pos F(front) or B(back), determining which half to take
+ * @return {array|number} [number, number] | number
  */
-function halfRange(range) {
+function getRange(range, dir) {
   const lowerTop = Math.floor((range[1] - range[0]) / 2);
-  return { lower: [range[0], lowerTop], upper: [lowerTop + 1, range[1]] };
+
+  if (lowerTop === 0) {
+    if (dir === "F") {
+      return range[0];
+    } else {
+      return range[1];
+    }
+  } else {
+    if (dir === "F") {
+      return [range[0], lowerTop + range[0]];
+    } else {
+      return [lowerTop + 1 + range[0], range[1]];
+    }
+  }
 }
 
-assert.deepStrictEqual(halfRange([0, 127]), {
-  lower: [0, 63],
-  upper: [64, 127],
-});
+// ASSERTIONS
+assert.deepStrictEqual(getRange([0, 127], 'F'), [0, 63]);
+assert.deepStrictEqual(getRange([0, 127], 'B'), [64, 127]);
+assert.deepStrictEqual(getRange([44, 45], 'F'), 44);
+assert.deepStrictEqual(getRange([44, 45], 'B'), 45);
+assert.deepStrictEqual(getRange([32, 47], 'B'), [40, 47]);
+assert.deepStrictEqual(getRange([44, 47], 'F'), [44, 45]);
+
 // first seven letters are F or B
 //! need to split into tuple (7, 3)
 
@@ -44,4 +51,20 @@ assert.deepStrictEqual(halfRange([0, 127]), {
  * @param {string} letters
  * @return {number}
  */
-function findRow(letters) {}
+function findRow(letters) {
+  const lettersArr = letters.split("");
+  const defaultRange = [0, 127];
+  let currentRange = [];
+  
+  for (let i = 0; i < lettersArr.length; i++) {
+    if (i === 0) {
+      currentRange = getRange(defaultRange, lettersArr[i]);
+    } else {
+      currentRange = getRange(currentRange, lettersArr[i]);
+    }
+  }
+  return currentRange;
+}
+
+console.log(findRow('FBFBBFF'));
+

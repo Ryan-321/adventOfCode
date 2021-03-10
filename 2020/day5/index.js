de68bd5384a99c83c5030bac526ce6106c38074f
Ryan Kahle <jryankahle@gmail.com>
2021-03-09 20:40:14 -0500

    solve part one

--- 2020/day5/index.js
+++ 2020/day5/index.js
@@ -1,9 +1,10 @@
-const fs = require("fs");
-const assert = require("assert").strict;
+// binary space partitioning
+const fs = require('fs');
+const assert = require('assert').strict;
 
 const boardingPasses = fs
-  .readFileSync("./2020/day5/input.txt", "utf-8")
-  .split("\n")
+  .readFileSync('./2020/day5/input.txt', 'utf-8')
+  .split('\n')
   .map((str) => {
     const row = str.slice(0, 7);
     const column = str.slice(7);
@@ -12,50 +13,52 @@ const boardingPasses = fs
 
 /**
  * Takes a range and returns the upper and lower range depending on direction or returns the file row number
- * 
+ *
  * @param {array} range number[]
  * @param {string} pos F(front) or B(back), determining which half to take
- * @return {array|number} [number, number] | number
+ * @return {array|number} number[] | number
  */
 function getRange(range, dir) {
-  const lowerTop = Math.floor((range[1] - range[0]) / 2);
+  const half_the_differnce = Math.floor((range[1] - range[0]) / 2);
 
-  if (lowerTop === 0) {
-    if (dir === "F") {
+  if (half_the_differnce === 0) {
+    if (dir === 'F' || dir === 'L') {
       return range[0];
     } else {
       return range[1];
     }
   } else {
-    if (dir === "F") {
-      return [range[0], lowerTop + range[0]];
+    if (dir === 'F' || dir === 'L') {
+      return [range[0], half_the_differnce + range[0]];
     } else {
-      return [lowerTop + 1 + range[0], range[1]];
+      return [half_the_differnce + 1 + range[0], range[1]];
     }
   }
 }
 
-// ASSERTIONS
+// ASSERTIONS getRange
 assert.deepStrictEqual(getRange([0, 127], 'F'), [0, 63]);
 assert.deepStrictEqual(getRange([0, 127], 'B'), [64, 127]);
 assert.deepStrictEqual(getRange([44, 45], 'F'), 44);
 assert.deepStrictEqual(getRange([44, 45], 'B'), 45);
 assert.deepStrictEqual(getRange([32, 47], 'B'), [40, 47]);
 assert.deepStrictEqual(getRange([44, 47], 'F'), [44, 45]);
+assert.deepStrictEqual(getRange([0, 7], 'R'), [4, 7]);
+assert.deepStrictEqual(getRange([4, 7], 'L'), [4, 5]);
+assert.deepStrictEqual(getRange([4, 5], 'R'), 5);
 
 // first seven letters are F or B
 //! need to split into tuple (7, 3)
-
 /**
- * Given the first 7 letters of a boarding pass it will find the row
+ * Decodes column and row letters into their corresponding number given a range
  * @param {string} letters
+ * @param {number[]} defaultRange
  * @return {number}
  */
-function findRow(letters) {
-  const lettersArr = letters.split("");
-  const defaultRange = [0, 127];
+function findSeat(letters, defaultRange) {
+  const lettersArr = letters.split('');
   let currentRange = [];
-  
+
   for (let i = 0; i < lettersArr.length; i++) {
     if (i === 0) {
       currentRange = getRange(defaultRange, lettersArr[i]);
@@ -65,6 +68,16 @@ function findRow(letters) {
   }
   return currentRange;
 }
+// ASSERTIONS findSeat
+assert.deepStrictEqual(findSeat('FBFBBFF', [0, 127]), 44);
+assert.deepStrictEqual(findSeat('RLR', [0, 7]), 5);
 
-console.log(findRow('FBFBBFF'));
+const highest = boardingPasses
+  .map(([rowLetters, columnLetters]) => {
+    const row = findSeat(rowLetters, [0, 127]);
+    const column = findSeat(columnLetters, [0, 7]);
+    return row * 8 + column;
+  })
+  .sort((a, b) => b - a);
 
+console.log(highest[0]);

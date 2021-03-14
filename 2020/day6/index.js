f354898c9118129abf3f9fabae276935ff52a482
Ryan Kahle <jryankahle@gmail.com>
2021-03-14 07:46:57 -0400

    solves part two

--- 2020/day6/index.js
+++ 2020/day6/index.js
@@ -24,26 +24,66 @@ function countAffirmationsPerGroup(forms) {
 assert.deepStrictEqual(countAffirmationsPerGroup(['abcx', 'abcy', 'abcz']), 6);
 
 /**
+ * counts a group answers to which everyone says yes
+ * @param {string[]} forms
+ * @return {number}
+ */
+function countAnswersPerGroup(forms) {
+  const formsLen = forms.length;
+  let answersCount = 0;
+  let memory = {};
+  forms.forEach((form) => {
+    [...form].forEach((letter) => {
+      if (memory[letter]) {
+        memory[letter] += 1;
+      } else {
+        memory[letter] = 1;
+      }
+    });
+  });
+
+  for (const letter in memory) {
+    if (memory[letter] === formsLen) {
+      answersCount++;
+    }
+  }
+  return answersCount;
+}
+
+/**
  * takes all the groups of forms and sums all the groups counts
  * @param {string[][]} groups nested array of strings
  * @return {number}
  */
 function countGroups(groups) {
   return groups.reduce((acc, group) => {
-    const count = countAffirmationsPerGroup(group);
+    // const count = countAffirmationsPerGroup(group);  // PART ONE
+    const count = countAnswersPerGroup(group); // PART TWO
     acc += count;
     return acc;
   }, 0);
 }
-
+// PART TWO
 assert.deepStrictEqual(
   countGroups([
     ['abc'],
     ['a', 'b', 'c'],
     ['ab', 'ac'],
-    ['a', 'a', 'a', 'a', 'b'],
+    ['a', 'a', 'a', 'a'],
+    ['b'],
   ]),
-  11,
+  6,
 );
+
+// PART ONE
+// assert.deepStrictEqual(
+//   countGroups([
+//     ['abc'],
+//     ['a', 'b', 'c'],
+//     ['ab', 'ac'],
+//     ['a', 'a', 'a', 'a', 'b'],
+//   ]),
+//   11,
+// );
 
 console.log(countGroups(formsGrouped));

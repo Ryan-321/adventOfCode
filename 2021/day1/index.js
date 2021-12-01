50bc7c73c585acc162d1d1cd55b5b2522ea126df
Ryan Kahle <ryan@triblio.com>
2021-12-01 08:36:51 -0500

    day1 - puzzle two - solved, yay

--- 2021/day1/index.js
+++ 2021/day1/index.js
@@ -8,13 +8,30 @@ const inputs = fs.readFileSync('./2021/day1/input.txt', 'utf-8')
   .split('\n')
   .map(Number);
 
-let amountOfIncreases = 0;
-let prevMeasurement = inputs[0];
-const measurements = inputs.slice(1);
-measurements.forEach((m) => {  
-  if(prevMeasurement < m) {
-    amountOfIncreases++;
-  }
-  prevMeasurement = m;
+function findIncreases(nums) {
+  let amountOfIncreases = 0;
+  let prevMeasurement = nums[0];
+  const measurements = nums.slice(1);
+
+  measurements.forEach((m) => {  
+    if(prevMeasurement < m) {
+      amountOfIncreases++;
+    }
+    prevMeasurement = m;
+  });
+  return amountOfIncreases;
+}
+// console.log('puzzle one', { amountOfIncreases: findIncreases(inputs) });
+
+// * 2nd puzzle
+const measurements2 = inputs.map((num, idx) => {
+  const idxPlus1 = inputs[idx + 1] || 0;
+  const idxPlus2 = inputs[idx + 2] || 0;
+  const sum = num + idxPlus1 + idxPlus2;
+
+  console.log({ sum });
+  return sum;
 });
-console.log({ amountOfIncreases });
+
+console.log({ measurements2 });
+console.log('puzzle 2', { amountOfIncreases: findIncreases(measurements2)});

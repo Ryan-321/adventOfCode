40baf6aba69eeec55dcf95a5cdb41f8110954d7d
Ryan Kahle <ryan@triblio.com>
2021-12-03 06:13:09 -0500

    day2 solved

--- 2021/day2/index.js
+++ 2021/day2/index.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
-// notes
+// notes - puzzle one
   // forward is horizontal
   // down increases depth - positive
   // up decreases depth - negative
@@ -8,20 +8,52 @@ const fs = require('fs');
 const inputs = fs.readFileSync('./2021/day2/input.txt', 'utf-8')
   .split('\n');
 
-const position = { h: 0, d: 0 };
+function puzzleOne(inputs) {
+  const position = { h: 0, d: 0 };
 
-inputs.forEach((input) => {
-  const [dir, n] = input.split(' ');
-  const num = Number(n);
-  if(dir === 'forward') {
-    position.h += num;
-  } else if(dir === 'down') {
-    position.d += num;
-  } else if(dir === 'up') {
-    position.d -= num;
-  }
-});
+  inputs.forEach((input) => {
+    const [dir, n] = input.split(' ');
+    const num = Number(n);
+    if(dir === 'forward') {
+      position.h += num;
+    } else if(dir === 'down') {
+      position.d += num;
+    } else if(dir === 'up') {
+      position.d -= num;
+    }
+  });
+  return position.d * position.h;
+}
 
-console.log({ position });
-console.log('position product', position.d * position.h);
+// console.log('puzzle one', puzzleOne(inputs));
+
+
+// notes - puzzle one
+  // forward 
+  // down increases aim - positive
+  // up decreases aim - negative
+
+function puzzleTwo(inputs) {
+  const position = { h: 0, d: 0, a: 0 };
+
+  inputs.forEach((input) => {
+    const [dir, n] = input.split(' ');
+    const num = Number(n);
+
+    if(dir === 'forward') {
+      position.h += num;
+      position.d += position.a * n;
+    } else if(dir === 'down') {
+      position.a += num;
+    } else if(dir === 'up') {
+      position.a -= num;
+    }
+    console.log({ input });
+    console.log({ position });
+    console.log('***************');
+  });
+  return position.h * position.d;
+}
+
+console.log('puzzle two', puzzleTwo(inputs));
 
